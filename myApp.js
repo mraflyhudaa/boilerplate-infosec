0a05f2b9e08d2fe079e6253a7b790176cad5c338
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-11-16 13:21:56 +0700

    add helmet version

--- myApp.js
+++ myApp.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
+const helmetVersion = require("helmet/package.json").version;
 
 const app = express();
 
 app.use(helmet());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/_api/package.json", (req, res) => {
+  res.send(helmetVersion);
 });
 
 app.listen(3000, () => {
