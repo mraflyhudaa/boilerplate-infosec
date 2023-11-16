eb46c372454406221d2f34531337cec800e3d1ae
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-11-16 13:33:00 +0700

    dir

--- myApp.js
+++ myApp.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
-const package = require("./package.json");
 
 const app = express();
 
 app.use(helmet());
 
-app.get("/_api/package.json", (req, res) => {
-  console.log(package);
-  res.send(package);
-});
-
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.get("/_app/package.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "package.json"));
 });
